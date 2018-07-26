4fd8a23acff16dc398a8b0d27f9edc4868c6f2c3
Beaterie <josef.roth@uni-weimar.de>
2018-07-26 13:39:03 +0200

    Grundgerüst

---
+++ audiotechnik/Spur.swift
@@ -0,0 +1,27 @@
+//
+//  Spur.swift
+//  audiotechnik
+//
+//  Created by Jana Puschmann und Josef Roth on 01.07.18.
+//  Copyright © 2018 Jana Puschmann und Josef Roth. All rights reserved.
+//
+
+
+import Foundation
+
+class Spur {
+    
+    // Membervariablen
+    var name: String
+    var dauer: String
+    //var player
+    // ---------------------------------
+    
+    
+    // Konstruktor
+    init(name: String, dauer: String) {
+        self.name = name
+        self.dauer = dauer
+    }
+    // ---------------------------------
+}
